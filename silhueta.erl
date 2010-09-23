ebc783445848b1358a5e79f921b7e657244ef9f6
Thiago Colucci <thiagorcolucci@gmail.com>
2010-09-23 18:29:05 -0300

    algoritmo1 funcionando!

--- silhueta.erl
+++ silhueta.erl
@@ -3,10 +3,14 @@
 -compile(export_all).
 
 
-silhueta_de_edificio({X_inicial, Altura, X_final}) ->
+silhueta_do_edificio({X_inicial, Altura, X_final}) ->
     [{X_inicial, Altura}, {X_final, 0}].
 
 
-%uniao(Silhueta_1, Silhueta_2)
+algoritmo1(Lista_de_edificios) ->
+    algoritmo1(Lista_de_edificios, []).
 
-
+algoritmo1([], Resultado) ->
+    Resultado;
+algoritmo1([Edificio | Resto], Resultado) ->
+    algoritmo1(Resto, uniao:uniao(silhueta_do_edificio(Edificio), Resultado)).
--- silhueta_tests.erl
+++ silhueta_tests.erl
@@ -1,6 +1,31 @@
 -module(silhueta_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-silhueta_de_edificio_test() ->
-    ?assertEqual([{0, 1}, {3, 0}], silhueta:silhueta_de_edificio({0, 1, 3})),
-    ?assertEqual([{1, 2}, {4, 0}], silhueta:silhueta_de_edificio({1, 2, 4})).
+silhueta_do_edificio_test() ->
+    ?assertEqual([{0, 1}, {3, 0}], silhueta:silhueta_do_edificio({0, 1, 3})),
+    ?assertEqual([{1, 2}, {4, 0}], silhueta:silhueta_do_edificio({1, 2, 4})).
+
+
+algoritmo_1_caso_1_test() ->
+    ?assertEqual([{0, 1}, {1, 0}, {2, 1}, {3, 0}], 
+		 silhueta:algoritmo1([{0, 1, 1},
+				       {2, 1, 3}])).
+
+algoritimo_1_caso_8_test() ->
+    ?assertEqual([{0, 1}, {1, 0}, {2, 1}, {3, 0}, {4, 1}, {5, 0}],
+		 silhueta:algoritmo1([{0, 1, 1},
+				       {4, 1, 5},
+				       {2, 1, 3}])).
+    
+
+
+algoritmo_2_caso_1_test() ->
+    ?assertEqual([{0, 1}, {1, 0}, {2, 1}, {3, 0}], 
+		 silhueta:algoritmo2([{0, 1, 1},
+				       {2, 1, 3}])).
+
+algoritimo_2_caso_8_test() ->
+    ?assertEqual([{0, 1}, {1, 0}, {2, 1}, {3, 0}, {4, 1}, {5, 0}],
+		 silhueta:algoritmo2([{0, 1, 1},
+				       {4, 1, 5},
+				       {2, 1, 3}])).
